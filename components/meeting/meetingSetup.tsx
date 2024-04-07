dd45c4d8ebb2e48779c38058ffe0e02bb735fe49
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-04-07 19:14:24 +0800

    feat: added meeting preview

--- components/meeting/meetingSetup.tsx
+++ components/meeting/meetingSetup.tsx
@@ -1,5 +1,35 @@
+'use client';
+
+import { VideoPreview, useCall } from '@stream-io/video-react-sdk';
+import { useEffect, useState } from 'react';
+
 const MeetingSetup = () => {
-	return <div>meetingSetup</div>;
+	// init state
+	const [isMicCamToggleOn, setIsMicCamToggleOn] = useState(false);
+
+	const call = useCall();
+
+	if (!call) {
+		throw new Error('useCall must be used within StreamCall component.');
+	}
+
+	useEffect(() => {
+		if (isMicCamToggleOn) {
+			call?.camera.disable();
+			call?.microphone.disable();
+		} else {
+			call?.camera.enable();
+			call?.microphone.enable();
+		}
+	}, [isMicCamToggleOn, call?.camera, call?.microphone]);
+
+	return (
+		<div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
+			<h1 className='text-2xl font-bold'>Setup</h1>
+
+			<VideoPreview />
+		</div>
+	);
 };
 
 export default MeetingSetup;
